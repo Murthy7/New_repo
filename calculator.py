24d789acb9698ab764a75dfb8ca789ec886ff8c4
Murthy7 <95077952+Murthy7@users.noreply.github.com>
2021-12-01 19:16:59 +0530

    calculator prog

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("Hi..! Welcome to calculator..!")
+print("MENU")
+print("For addition press: 1\nFor substraction press: 2\nFor Multiplication press: 3\nFor Division press: 4")
+user= int(input("press valid option: "))
+a= int(input("enter the number: "))
+b= int(input("enter the number: "))
+if user == 1:
+    print("Addition :",a+b)
+elif user == 2:
+    print("substraction :",a-b)
+elif user == 3:
+    print("Multiplication :",a*b)
+elif user == 4:
+    print("Division :",a/b)
+print("Thank you for using calculator..!!")
+
+
+
+
+
+
